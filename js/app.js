2dcb5305cfc98c1a47fab5d17f7ad576d6ec7cc2
Ehsan Ghafari <ehsanghafari88@gmail.com>
2019-09-08 12:30:33 -0700

    added more JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 document.getElementById('shareBtn').addEventListener("click", function(){
-  var body = Array.from(document.getElementById('main-text').getElementsByTagName('p')).map(p => p.textContent).join('\n');
+  var bodyfirst = Array.from(document.getElementById('main-text').getElementsByTagName('p')).map(p => p.textContent).join('\n');
+  var bodysecond = Array.from(document.getElementById('recipe').getElementsByTagName('li')).map(p => p.textContent).join('\n');
   var subject = 'Chocolate Pizza Recipe';
 
-  location.href=`mailto:?subject=${subject}&body=${body}`;
+  location.href=`mailto:?subject=${subject}&body=${bodyfirst} - ${bodysecond}`;
 });
