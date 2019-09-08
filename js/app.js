42f6e03dcc34c5f32d6deed43d790f2189cb798b
Ehsan Ghafari <ehsanghafari88@gmail.com>
2019-09-08 12:14:53 -0700

    add javascript

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 'use strict';
 
+document.getElementById('shareBtn').addEventListener("click", function(){
+  var body = Array.from(document.getElementById('main-text').getElementsByTagName('p')).map(p => p.textContent).join('\n');
+  var subject = 'Chocolate Pizza Recipe';
+
+  location.href=`mailto:?subject=${subject}&body=${body}`;
+});
